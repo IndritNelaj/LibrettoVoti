7578534aa0f453a91764b24778318d738f22aa3c
indrit <indrit@DESKTOP-QGDP7DE>
2021-04-09 13:39:42 +0200

    Soluzione punto 3

--- src/main/java/it/polito/tdp/LibrettoVoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/LibrettoVoti/model/Libretto.java
@@ -13,8 +13,50 @@ public class Libretto {
 	public void add(Voto v) {
 		this.voti.add(v);
 	}
-
-	@Override
+	/* non vanno bene!!
+	 * 
+	public void stampaVotiUguali(int punteggio) {
+		// stampa solo voti con quel punteggio
+	}
+	
+	public String votiUguali (int punteggio) {
+		// crea una stringa che contiene i voti
+		// sara poi il chiamante a stamparli
+		// solo il nome ? 
+		// tutto il Voto.toString() ?
+	}
+	 */
+	public List<Voto> listaDiVotiUguali (int punteggio){
+		// restituisce solo i voti uguali al punteggio
+		List <Voto> risultato = new ArrayList<>();
+		for(Voto v: this.voti) {
+			if(v.getVoto()==punteggio)
+			risultato.add(v);
+		}
+		return risultato;
+	}
+	
+	public Libretto votiUguali1 (int punteggio) {
+		// restituisce un libretto con i voti filtrati per quel punteggio specifico
+		// il libretto ha  gia tutti i metodi che servono
+		Libretto risultato = new Libretto();
+		for(Voto v: this.voti) {
+			if(v.getVoto()==punteggio)
+				risultato.add(v);
+		}
+		return risultato;
+	}
+	
+	public Voto cercaCorso (String nomeCorso) {
+		Voto voto = null;
+		for(Voto v: this.voti) {
+			if(v.getNome().equals(nomeCorso)) {
+				voto=v;
+			break;
+			}
+		}
+		return voto;
+	}
 	public String toString() {
 		String r = "";
 		for(Voto v : voti) {
--- src/main/java/it/polito/tdp/LibrettoVoti/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/LibrettoVoti/model/TestLibretto.java
@@ -1,6 +1,7 @@
 package it.polito.tdp.LibrettoVoti.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class TestLibretto {
 	public static void main(String[] args) {
@@ -9,8 +10,18 @@ public class TestLibretto {
 		Voto voto1 = new Voto("Analisi 1", 30, LocalDate.of(2019, 2, 15));
 		libretto.add(voto1);
 		libretto.add(new Voto("Fisica 1",28,LocalDate.of(2019, 7, 15)));
+		libretto.add(new Voto("Chimica",25,LocalDate.of(2019, 6, 17)));
 		libretto.add(new Voto("Informatica",24,LocalDate.of(2019, 9, 15)));
 		
 		System.out.println(libretto);
+		List<Voto> venticinque = libretto.listaDiVotiUguali(25);
+		System.out.println(venticinque);
+		
+		Libretto librettoVenticinque = libretto.votiUguali1(25);
+		System.out.println(librettoVenticinque);
+		
+		Voto cercavoto= libretto.cercaCorso("Informatica");
+		System.out.println("il voto è : " + cercavoto.getVoto());
+		
 	}
 }
