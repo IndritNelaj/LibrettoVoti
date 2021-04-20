8c3c3683cfcab35f84579ca84d2a0934afa465a3
indrit <indrit@DESKTOP-QGDP7DE>
2021-04-20 18:07:37 +0200

    20/04/2021

--- src/main/java/it/polito/tdp/LibrettoVoti/FXMLController.java
+++ src/main/java/it/polito/tdp/LibrettoVoti/FXMLController.java
@@ -1,47 +1,70 @@
 package it.polito.tdp.LibrettoVoti;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 import it.polito.tdp.LibrettoVoti.model.Libretto;
+import it.polito.tdp.LibrettoVoti.model.Voto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class FXMLController {
-	private Libretto model; 
-    @FXML
-    private ResourceBundle resources;
+	private Libretto model;
+	@FXML
+	private ResourceBundle resources;
 
-    @FXML
-    private URL location;
+	@FXML
+	private URL location;
 
-    @FXML
-    private TextField txtEsame;
+	@FXML
+	private TextField txtEsame;
 
-    @FXML
-    private TextField txtVoto;
+	@FXML
+	private TextField txtVoto;
 
-    @FXML
-    private TextField txtData;
+	@FXML
+	private TextField txtData;
 
-    @FXML
-    private TextArea txtResult;
+	@FXML
+	private TextArea txtResult;
 
-    @FXML
-    void HandleInserisci(ActionEvent event) {
+	@FXML
+	void HandleInserisci(ActionEvent event) {
+		// controlla e gestisci i dati inseriti
 
-    }
-    public void setModel(Libretto model) {
-    	this.model = model;
-    }
-    @FXML
-    void initialize() {
-        assert txtEsame != null : "fx:id=\"txtEsame\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert txtVoto != null : "fx:id=\"txtVoto\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert txtData != null : "fx:id=\"txtData\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+		String nomeEsame = txtEsame.getText();
+		if (nomeEsame.length() == 0) {
+			txtResult.setText("Errore - inserire il nome dell'esame !");
+			return;
+		}
+		String votoEsame = txtVoto.getText();
+		int votoInt = Integer.parseInt(votoEsame);
+		String dataEsame = txtData.getText();
+		LocalDate data = LocalDate.parse(dataEsame);
+		
 
-    }
+		// esegui l'azione
+		Voto voto = new Voto(nomeEsame, votoInt, data);
+		model.add(voto);
+
+		// aggiorna i dati nella view
+		txtResult.setText(model.toString());
+
+	}
+
+	public void setModel(Libretto model) {
+		this.model = model;
+	}
+
+	@FXML
+	void initialize() {
+		assert txtEsame != null : "fx:id=\"txtEsame\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert txtVoto != null : "fx:id=\"txtVoto\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert txtData != null : "fx:id=\"txtData\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+
+	}
 }
